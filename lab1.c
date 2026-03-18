98210f799455785b30355dab937c36640ca3456b
github_username_here <email@example.com>
2026-03-19 00:05:10 +0600

    first

--- lab1.c
+++ lab1.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, sum;
+int main() {
+    int n, i, j;
+    printf("Enter n: ");
+    scanf("%d", &n);
 
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    // ১. Star Diamond Pattern
+    printf("\n--- Star Diamond Pattern ---\n");
+    // উপরের অংশ
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++) printf("*");
+        printf("\n");
+    }
+    // নিচের অংশ
+    for (i = n - 1; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++) printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++) printf("*");
+        printf("\n");
+    }
 
-    sum = a + b;
-
-    printf("Sum = %d\n", sum);
+    // ২. Alphabetic Diamond Pattern
+    printf("\n--- Alphabetic Diamond Pattern ---\n");
+    // উপরের অংশ
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) printf(" ");
+        for (j = 0; j < 2 * i - 1; j++) printf("%c", 'A' + j);
+        printf("\n");
+    }
+    // নিচের অংশ
+    for (i = n - 1; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++) printf(" ");
+        for (j = 0; j < 2 * i - 1; j++) printf("%c", 'A' + j);
+        printf("\n");
+    }
 
     return 0;
 }
