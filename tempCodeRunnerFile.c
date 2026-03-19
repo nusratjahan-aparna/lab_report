a10e5ff20c20190716af49f17bdb5cf6fdf1949c
github_username_here <email@example.com>
2026-03-19 17:21:22 +0600

    add the folder

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0;
+    int inWord = 0;
+
+    fp = fopen("text.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("File cannot be opened!\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        characters++;
+
+        // Check for word boundaries
+        if (isspace(ch))
+        {
+            inWord = 0;
+        }
+        else if (inWord == 0)
+        {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Number of characters: %d\n", characters);
+    printf("Number of words: %d\n", words);
+
+    return 0;
+}
